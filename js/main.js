d1e4de8c9a660de741148a5139468d7d5350443b
macb <macb88@gmail.com>
2017-04-23 22:40:19 +0200

    Change way of calculating

--- js/main.js
+++ js/main.js
@@ -1,9 +1,73 @@
 $(document).ready(function() {
-	var number = "";
+	var timeStart = 0;
+	var timeEnd = 0;
+	var timeInput = '';
+	var hoursMinutes = 0;
+
+	function displayHours(number) {
+		timeInput += number;
+		if (timeInput.slice(0,1) == 0) {
+			timeInput = timeInput.slice(1);
+			return '0';
+		} else {
+			return timeInput;		
+		}
+	};
+
+	function displayMinutes(number) {
+		timeInput += number;
+		if (timeInput.slice(0,1) == 0) {
+			timeInput = timeInput.slice(1);
+			return '0';
+		} else {
+			return timeInput;		
+		}
+	};
+
+	$('#hoursMinutes').on('click', function() {
+		hoursMinutes == 0 ? hoursMinutes = 1 : hoursMinutes = 0;
+		timeInput = '';
+	});
+
+	$('button').on('click', function() {
+		if (($.isNumeric($(this).val())) && (hoursMinutes == 0)) {
+			$('#hours').text(displayHours($(this).val()));
+		} else if (($.isNumeric($(this).val())) && (hoursMinutes == 1)) {
+			$('#minutes').text(displayMinutes($(this).val()));
+		};
+	});
+
+	/*var i = 0;
+
+	function addDigit(digit) {
+		if ((i == 0) && ($.isNumeric(digit)) && (digit != 0)) {
+			$('#display').text('0' + digit);
+			i++;
+		} else if ((i == 1) && ($.isNumeric(digit))) { 
+			display = $('#display').text();
+			display = display.substr(1);
+			$('#display').text(display);
+			$('#display').append(digit);
+			i++;
+		} else if ((i > 1) && ($.isNumeric(digit))) {
+			$('#display').append(digit);
+		}
+	};
+	
+	$('button').on('click', function() {
+		clicked_digit = $(this).val();
+		console.log(clicked_digit);
+		console.log($.isNumeric(clicked_digit));
+		console.log('i=' + i);
+		addDigit(clicked_digit);
+	});
+	*/
+
+	/*
 	$('button').click(function() {
 		number = $(this).val();
 		console.log(number);
 		$('#display').append(number);
 	});
-	
+	*/
 });
